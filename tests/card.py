39cb9c95ded7d2a931c87151ecffc47a7fc40f07
mizuki sato <mizki.sato.0025@gmail.com>
2019-06-23 16:09:36 +0900

    カードの引数の値チェック

--- tests/card.py
+++ tests/card.py
@@ -1,6 +1,12 @@
 class Card:
 
     def __init__(self, suit: str, rank: str):
+        suit_list = ['♠', '♥', '♣', '♦︎']
+        if suit not in suit_list:
+            raise ValueError(f'{suit} is invalid suit')
+        if not isinstance(rank, str):
+            raise TypeError(f'TypeError: unsupported type for rank: "str" , not {type(rank)}')
+
         self._suit = suit
         self._rank = rank
 
--- tests/test_card.py
+++ tests/test_card.py
@@ -1,7 +1,16 @@
+import pytest
 from card import Card
 
 
 def test_get_notation():
-    card = Card('♠', 2)
+    card = Card('♠', '2')
 
     assert '♠2' == str(card)
+
+def test_invalid_value_of_suit():
+    with pytest.raises(ValueError):
+        Card('ほげ', '2')
+
+def test_invalid_type_of_rank():
+    with pytest.raises(TypeError):
+        Card('♠', 2)
