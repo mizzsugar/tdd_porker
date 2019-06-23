57239d94e15258fe702dbce2c87dbeffc5887be7
itsumura-h <dumblepy@gmail.com>
2019-06-23 18:00:00 +0900

    init

--- tests/card.py
+++ tests/card.py
@@ -1,27 +1,33 @@
 import enum
 
-class Suit(enum.Enum):
-    space = '♠'
+class Suit:
+    def __init__(self, value):
+        suit_list = ['♠', '♥', '♣', '♦︎']
+        if value not in suit_list:
+            raise ValueError(f'{value} is invalid suit')
 
+        self.value = value
 
-class Rank(enum.Enum):
-    two = '2'
+    def __str__(self) -> str:
+        return f"{self.value}"
 
+class Rank:
+    def __init__(self, value):
+        rank_list = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+        if not isinstance(value, str):
+            raise TypeError(f'TypeError: unsupported type for rank: "str" , not {type(value)}')
+
+        if value not in rank_list:
+            raise ValueError(f'{value} is invalid rank')
+
+        self.value = value
+
+    def __str__(self) -> str:
+        return f"{self.value}"
 
 class Card:
 
     def __init__(self, suit: str, rank: str):
-        suit_list = ['♠', '♥', '♣', '♦︎']
-        rank_list = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-        if suit not in suit_list:
-            raise ValueError(f'{suit} is invalid suit')
-
-        if not isinstance(rank, str):
-            raise TypeError(f'TypeError: unsupported type for rank: "str" , not {type(rank)}')
-
-        if rank not in rank_list:
-            raise ValueError(f'{rank} is invalid rank')
-
         self._suit = suit
         self._rank = rank
 
@@ -29,4 +35,4 @@ class Card:
         return f"{self._suit}{self._rank}"
 
 if __name__ == "__main__":
-    card = Card(suit="♠", rank=2)
+    card = Card(suit, rank)
--- tests/test_card.py
+++ tests/test_card.py
@@ -1,26 +1,46 @@
 import pytest
 from card import Card, Rank, Suit
 
-
-def test_get_notation():
-    card = Card('♠', '2')
-
-    assert '♠2' == str(card)
-
-def test_get_notation_2():
-    card = Card(Suit.spade, Rank.two)
-
-def test_get_notation_3():
-    card = Card(Suit('♠'), Rank('2'))
-
 def test_invalid_value_of_suit():
     with pytest.raises(ValueError):
-        Card('ほげ', '2')
+        suit = Suit('すぺーど')
 
 def test_invalid_type_of_rank():
     with pytest.raises(TypeError):
-        Card('♠', 2)
+        rank = Rank(1)
 
-def test_invalid_value_of_rank():
+def test_invalid_type_of_rank():
     with pytest.raises(ValueError):
-        Card('♠', '14')
+        rank = Rank('11')
+
+def test_card_ok():
+    suit = Suit('♠')
+    rank = Rank('2')
+    card = Card(suit, rank)
+
+    assert '♠2' == str(card)
+
+
+
+# def test_get_notation():
+#     card = Card('♠', '2')
+
+#     assert '♠2' == str(card)
+
+# def test_get_notation_2():
+#     card = Card(Suit.spade, Rank.two)
+
+# def test_get_notation_3():
+#     card = Card(Suit('♠'), Rank('2'))
+
+# def test_invalid_value_of_suit():
+#     with pytest.raises(ValueError):
+#         Card('ほげ', '2')
+
+# def test_invalid_type_of_rank():
+#     with pytest.raises(TypeError):
+#         Card('♠', 2)
+
+# def test_invalid_value_of_rank():
+#     with pytest.raises(ValueError):
+#         Card('♠', '14')
